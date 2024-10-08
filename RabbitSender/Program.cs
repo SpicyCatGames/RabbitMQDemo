2c8c868b4d963884e6de1e179b43839a6cac44f0
Md Sadman Chowdhury <61442059+SpicyCatGames@users.noreply.github.com>
2024-10-08 16:13:01 +0600

    Initial commit

---
+++ RabbitSender/Program.cs
@@ -0,0 +1,17 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Sender App";
+
+IConnection cnn = factory.CreateConnection();
+
+IModel channel = cnn.CreateModel();
+
+string exchangeName = "DemoExchange";
+string routingKey = "DemoRoutingKey";
+string queueName = "DemoQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, false, false, false, null);
